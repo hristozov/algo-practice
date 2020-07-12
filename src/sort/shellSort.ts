c85492476bde39d1d888603455bf64002db55a82
Georgi Hristozov <georgi@forkbomb.nl>
2020-07-12 14:11:56 +0300

    Add some comments for Shellsort.

--- src/sort/shellSort.ts
+++ src/sort/shellSort.ts
@@ -2,8 +2,39 @@ import { cloneArray } from '../util';
 
 import { Comparator, ISort } from './sort';
 
-const GAPS = [127, 63, 31, 15, 7, 3, 1]; // Hibbard, 2^k - 1
+/**
+ * 2^k - 1, Hibbard (1963)
+ */
+const GAPS = [
+  1048575,
+  524287,
+  262143,
+  131071,
+  65535,
+  32767,
+  16383,
+  8191,
+  4095,
+  2047,
+  1023,
+  511,
+  255,
+  127,
+  63,
+  31,
+  15,
+  7,
+  3,
+  1,
+];
 
+/**
+ * Implementation of Shellsort.
+ *
+ * Inner loop uses insertion sort.
+ *
+ * https://en.wikipedia.org/wiki/Shellsort
+ */
 export class ShellSort<T> implements ISort<T> {
   public sort(t: T[], comparator: Comparator<T>): T[] {
     const result = cloneArray(t);
